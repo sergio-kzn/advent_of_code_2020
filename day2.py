35ad85348afccd46554fb99e9bc0cef3cc97b4db
sergio-kzn <sergio@dezigner.ru>
2020-12-05 11:24:03 +0300

    Добавлены описания заданий

--- day2.py
+++ day2.py
@@ -18,6 +18,32 @@ Each line gives the password policy and then the password. The password policy i
 In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
 
 How many passwords are valid according to their policies?
+
+Your puzzle answer was 393.
+
+--- Part Two ---
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+How many passwords are valid according to the new interpretation of the policies?
+
+Your puzzle answer was 690.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
+At this point, you should return to your Advent calendar and try another puzzle.
+
+If you still want to see it, you can get your puzzle input.
+
+You can also [Share] this puzzle.
 """
 
 import data
@@ -47,22 +73,6 @@ for item in data:
             true_passwords += 1
 
     part_one()
-
-    """
-    --- Part Two ---
-    While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
-    
-    The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
-    
-    Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
-    
-    Given the same example list from above:
-    
-    1-3 a: abcde is valid: position 1 contains a and position 3 does not.
-    1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
-    2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
-    How many passwords are valid according to the new interpretation of the policies?
-    """
 
     def part_two():
         global true_passwords_2
--- day3.py
+++ day3.py
@@ -1,8 +1,8 @@
 """
 --- Day 3: Toboggan Trajectory ---
-With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees_2. You'll need to see which angles will take you near the fewest trees_2.
+With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
 
-Due to the local geology, trees_2 in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees_2 (#) you can see. For example:
+Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
 
 ..##.......
 #...#...#..
@@ -15,7 +15,7 @@ Due to the local geology, trees_2 in this area only grow on exact integer coordi
 #.##...#...
 #...##....#
 .#..#...#.#
-These aren't the only trees_2, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
+These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
 
 ..##.........##.........##.........##.........##.........##.......  --->
 #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
@@ -30,7 +30,7 @@ These aren't the only trees_2, though; due to something you read about once invo
 .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
 You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
 
-The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees_2 you would encounter for the slope right 3, down 1:
+The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
 
 From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
 
@@ -47,9 +47,35 @@ The locations you'd check in the above example are marked here with O where ther
 #.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
 #...##....##...##....##...#X....##...##....##...##....##...##....#
 .#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
-In this example, traversing the map using this slope would cause you to encounter 7 trees_2.
+In this example, traversing the map using this slope would cause you to encounter 7 trees.
 
-Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees_2 would you encounter?
+Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+
+Your puzzle answer was 145.
+
+--- Part Two ---
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+Right 1, down 1.
+Right 3, down 1. (This is the slope you already checked.)
+Right 5, down 1.
+Right 7, down 1.
+Right 1, down 2.
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+
+Your puzzle answer was 3424528800.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
+At this point, you should return to your Advent calendar and try another puzzle.
+
+If you still want to see it, you can get your puzzle input.
+
+You can also [Share] this puzzle.
 """
 
 import data
@@ -69,7 +95,6 @@ trees_5 = 0
 
 for index, line in enumerate(data):
     line *= 80
-    # 2935310400
     if index > 0:
         new_line = list(line)
         try:
@@ -111,21 +136,5 @@ print('trees_2 =', trees_2)
 print('trees_3 =', trees_3)
 print('trees_4 =', trees_4)
 print('trees_5 =', trees_5)
+
 print(trees_1 * trees_2 * trees_3 * trees_4 * trees_5)
-
-"""
---- Part Two ---
-Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
-
-Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
-
-Right 1, down 1.
-Right 3, down 1. (This is the slope you already checked.)
-Right 5, down 1.
-Right 7, down 1.
-Right 1, down 2.
-In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
-
-What do you get if you multiply together the number of trees encountered on each of the listed slopes?
-
-"""
